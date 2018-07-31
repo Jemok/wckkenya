f3e0ced96004be81b2e21bd54ccff69a616e371c
James Mwangi <karokijames40@gmail.com>
2018-07-31 20:39:22 +0300

    Changes

--- apply.php
+++ apply.php
@@ -18,7 +18,7 @@ $course    = @trim(stripslashes($_POST['course']));
 
 
 $email_from = $email;
-$email_to = 'info@wildlifeclubsofkenya.org';//replace with your email
+$email_to = 'khatushi33@gmail.com';//replace with your email
 
 $body = 'Course: ' . $course . 'Name: ' . "\n\n" . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message. "\n\n" . 'Phone: ' . $phone ."\n\n" . 'ID Number: ' . $id_number. "\n\n" . 'D.O.B: ' . $d_o_birth;
 
